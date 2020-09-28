ea3c15b308b27977b643471844dead3125423c42
liu <tomliu199517@gamil.com>
2020-09-28 21:32:22 +0800

    first commit

---
+++ SecViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecViewController.swift
+//  点击cell跳转Plain效果
+//
+//  Created by Liu&Jewel on 2020/9/28.
+//  Copyright © 2020 Liu&Jewel. All rights reserved.
+//
+
+import UIKit
+
+class SecViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
